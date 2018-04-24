02e9fb7f409d7e3d10f2471c28ac91730dcdf49b
sophia-jihye <jihyeparkk@naver.com>
2018-04-24 15:55:18 +0900

    commit

--- data-copier/src/sophia/copier/dto/TempOutputDto.java
+++ data-copier/src/sophia/copier/dto/TempOutputDto.java
@@ -4,6 +4,14 @@ public class TempOutputDto {
 
 	private String colcol1;
 	private String colcol2;
+	private String colcol3;
+	private String colcol4;
+	private String colcol5;
+	private String colcol6;
+	private String colcol7;
+	private String colcol8;
+	private String colcol9;
+	private String colcol10;
 	private String dttm;
 
 	public String getColcol1() {
@@ -20,6 +28,70 @@ public class TempOutputDto {
 
 	public void setColcol2(String colcol2) {
 		this.colcol2 = colcol2;
+	}
+
+	public String getColcol3() {
+		return colcol3;
+	}
+
+	public void setColcol3(String colcol3) {
+		this.colcol3 = colcol3;
+	}
+
+	public String getColcol4() {
+		return colcol4;
+	}
+
+	public void setColcol4(String colcol4) {
+		this.colcol4 = colcol4;
+	}
+
+	public String getColcol5() {
+		return colcol5;
+	}
+
+	public void setColcol5(String colcol5) {
+		this.colcol5 = colcol5;
+	}
+
+	public String getColcol6() {
+		return colcol6;
+	}
+
+	public void setColcol6(String colcol6) {
+		this.colcol6 = colcol6;
+	}
+
+	public String getColcol7() {
+		return colcol7;
+	}
+
+	public void setColcol7(String colcol7) {
+		this.colcol7 = colcol7;
+	}
+
+	public String getColcol8() {
+		return colcol8;
+	}
+
+	public void setColcol8(String colcol8) {
+		this.colcol8 = colcol8;
+	}
+
+	public String getColcol9() {
+		return colcol9;
+	}
+
+	public void setColcol9(String colcol9) {
+		this.colcol9 = colcol9;
+	}
+
+	public String getColcol10() {
+		return colcol10;
+	}
+
+	public void setColcol10(String colcol10) {
+		this.colcol10 = colcol10;
 	}
 
 	public String getDttm() {
