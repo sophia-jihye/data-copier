d966099c9923cfe4cae9dc634da426474162247a
sophia-jihye <jihyeparkk@naver.com>
2018-04-24 13:43:00 +0900

    commit

--- data-copier/src/sophia/copier/dto/TempOutputDto.java
+++ data-copier/src/sophia/copier/dto/TempOutputDto.java
@@ -2,23 +2,28 @@ package sophia.copier.dto;
 
 public class TempOutputDto {
 
-	private String bookNm;
-	private String bookPrice;
+	private String book_nm_2;
+	private String book_price_2;
 
-	public String getBookNm() {
-		return bookNm;
+	public String getBook_nm_2() {
+		return book_nm_2;
 	}
 
-	public void setBookNm(String bookNm) {
-		this.bookNm = bookNm;
+	public void setBook_nm_2(String book_nm_2) {
+		this.book_nm_2 = book_nm_2;
 	}
 
-	public String getBookPrice() {
-		return bookPrice;
+	public String getBook_price_2() {
+		return book_price_2;
 	}
 
-	public void setBookPrice(String bookPrice) {
-		this.bookPrice = bookPrice;
+	public void setBook_price_2(String book_price_2) {
+		this.book_price_2 = book_price_2;
+	}
+
+	@Override
+	public String toString() {
+		return "TempOutputDto [book_nm_2=" + book_nm_2 + ", book_price_2=" + book_price_2 + "]";
 	}
 
 }
