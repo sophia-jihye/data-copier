240ac171c12a559c102b1f38b4802d41fe1375e0
sophia-jihye <jihyeparkk@naver.com>
2018-04-24 15:03:38 +0900

    commit

--- data-copier/src/sophia/copier/dto/TempOutputDto.java
+++ data-copier/src/sophia/copier/dto/TempOutputDto.java
@@ -2,24 +2,24 @@ package sophia.copier.dto;
 
 public class TempOutputDto {
 
-	private String book_nm_2;
-	private String book_price_2;
+	private String colcol1;
+	private String colcol2;
 	private String dttm;
 
-	public String getBook_nm_2() {
-		return book_nm_2;
+	public String getColcol1() {
+		return colcol1;
 	}
 
-	public void setBook_nm_2(String book_nm_2) {
-		this.book_nm_2 = book_nm_2;
+	public void setColcol1(String colcol1) {
+		this.colcol1 = colcol1;
 	}
 
-	public String getBook_price_2() {
-		return book_price_2;
+	public String getColcol2() {
+		return colcol2;
 	}
 
-	public void setBook_price_2(String book_price_2) {
-		this.book_price_2 = book_price_2;
+	public void setColcol2(String colcol2) {
+		this.colcol2 = colcol2;
 	}
 
 	public String getDttm() {
@@ -28,11 +28,6 @@ public class TempOutputDto {
 
 	public void setDttm(String dttm) {
 		this.dttm = dttm;
-	}
-
-	@Override
-	public String toString() {
-		return "TempOutputDto [book_nm_2=" + book_nm_2 + ", book_price_2=" + book_price_2 + ", dttm=" + dttm + "]";
 	}
 
 }
